ddc516a16a2bfead86556a522f608dee0aeca707
BYIRINGIRO BRICE ALI <bricealibyilingiro@gmail.com>
2025-09-30 16:04:22 +0000

    Typo bug fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const cookieSession = require("cookie-session");
+const session = require("cookie-session");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const MongoStore = require("connect-mongo");
